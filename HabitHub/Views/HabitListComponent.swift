2bf7a5059bab611710965aa59219776bbf279c21
Andrea Bottino <bottinoandrea93@gmail.com>
2024-02-18 18:09:13 +0000

    Tweaked HabitListComponent

--- HabitHub/Views/HabitListComponent.swift
+++ HabitHub/Views/HabitListComponent.swift
@@ -12,13 +12,18 @@ struct HabitListComponent: View {
     let habit: HabitModel
     
     var body: some View {
-        HStack {
+        HStack(spacing: 20) {
             Image(systemName: habit.iconName)
-                .padding(10)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20, height: 20)
+                .padding()
                 .foregroundStyle(HabitColors.setColor(using: habit.color))
                 .background(HabitColors.setColor(using: habit.color).opacity(0.3))
                 .clipShape(Circle())
+            
             Text(habit.title)
+            
             Spacer()
         }
         .frame(maxWidth: .infinity)
